8b9fd8559cbd1658e2408388b2e13f8200e77622
Vamsee Krishna <vamseekrishna@Vamsees-MBP.lan>
2024-09-03 15:21:00 -0500

    added items class for connecting with database

--- src/main/java/com/example/ecommerce_application/items.java
+++ src/main/java/com/example/ecommerce_application/items.java
@@ -1,4 +1,14 @@
 package com.example.ecommerce_application;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class items {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
 }
