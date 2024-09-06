5baa530648a78ab2b6fb2d1f2b5caa5feda601cc
vamsee9201 <vamseekrishna9201@gmail.com>
2024-09-06 17:38:08 -0500

    created the item entity

--- src/main/java/com/example/ecommerce_application/entity/item.java
+++ src/main/java/com/example/ecommerce_application/entity/item.java
@@ -1,9 +1,69 @@
 package com.example.ecommerce_application.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="ecommerce_db")
 public class item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int product_id;
+
+    @Column(name = "name")
+    private String product_name;
+
+    @Column(name = "description")
+    private String description;
+
+    public item() {}
+
+    public item(int product_id, String product_name, String description) {
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.description = description;
+    }
+
+    public item(String product_name, String description) {
+        this.product_name = product_name;
+        this.description = description;
+    }
+
+    // defining getter and setters
+
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // define to string method
+
+    @Override
+    public String toString() {
+        return "item{" +
+                "product_id=" + product_id +
+                ", product_name='" + product_name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
